88f20d266da0368ea585264bbdf4feabba24a5a5
HassanSaeed7 <Hsaeed1007@gmail.com>
2022-04-02 22:59:59 -0700

    styles

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const tl = gsap.timeline({paused: true, defaults: {duration: 0.05 }});
 const container = document.querySelector('.container');
 const container2 = document.querySelector('.container2');
 const button  = document.querySelector('.btn')
+const hamburgerButton = document.querySelector('.hamburger')
+const nav = document.querySelector('.mobile-menu')
 
 tl.to(container, {clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'})
 tl.to(container, {clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)'}, '+=1.5')
@@ -13,6 +15,12 @@ container.addEventListener('mouseover', () => {
     tl.play()
 })
 
+//tl.to(container, {clipPath: 'polygon(6% 6%, 94% 6%, 94% 94%, 6% 94%)'})
+//tl.to(container, {clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)'}, '+=1.5')
+ 
 
-// tl.to(container, {clipPath: 'polygon(6% 6%, 94% 6%, 94% 94%, 6% 94%)'})
-// tl.to(container, {clipPath: 'polygon(94% 6%, 94% 6%, 94% 96%, 94% 94%)'}, '=+1')
+hamburgerButton.addEventListener('click', () => {
+    nav.classList.toggle('show')
+    hamburgerBtn.classList.toggle('is-active');
+})
+
